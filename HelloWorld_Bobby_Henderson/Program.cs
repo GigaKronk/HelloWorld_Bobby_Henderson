a426401788b140833ff9520d604d5cb525480354
bobby henderson <bobby.henderson@gctcok.edu>
2023-02-23 09:49:37 -0600

    Initial commit

---
+++ HelloWorld_Bobby_Henderson/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld_Bobby_Henderson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("what is your last name?");
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine($"Hello,{firstname} {lastname}!");
+        }
+    }
+}
